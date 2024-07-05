3b898944f93b926113e98c8d9cf767ab906bebf0
Harsheeta <harsheeta.kap@gmail.com>
2024-07-05 11:57:52 +0530

    final commit

--- app/_components/CategorySearch.jsx
+++ app/_components/CategorySearch.jsx
@@ -2,17 +2,21 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalApi from "../_utils/GlobalApi";
+import Image from "next/image";
+const baseUrl = "http://localhost:1337";
 
 function CategorySearch() {
+  const [categoryList, setCategoryList] = useState([]);
   useEffect(() => {
     getCategoryList();
   }, []);
 
   const getCategoryList = () => {
     GlobalApi.getCategory().then((resp) => {
-      console.log(resp);
+      console.log(resp.data.data);
+      setCategoryList(resp.data.data);
     });
   };
 
@@ -34,6 +38,33 @@ function CategorySearch() {
           <Search className="h-4 w-4 mr-2" />
           Search
         </Button>
+      </div>
+
+      <div className="grid grid-cols-3 mt-5 md:grid-cols-4 lg:grid-cols-6 ">
+        {categoryList.map((item, index) => (
+          <div
+            key={index}
+            className="flex 
+        flex-col text-center items-center
+        p-5 bg-blue-50 m-2 rounded-lg cursor-pointer
+        gap-2 hover:scale-110 transition-all ease-in-out"
+          >
+            <Image
+              src={
+                "http://localhost:1337/uploads/cardiology_f0f05c6eba.png"
+                // item.attributes?.Icon?.data.attributes?.url ||
+                // "/public/demo.png"
+              }
+              alt="icon"
+              width={40}
+              height={40}
+            />
+
+            <label className="text-blue-600 text-sm">
+              {item?.attributes?.Name}
+            </label>
+          </div>
+        ))}
       </div>
     </div>
   );
